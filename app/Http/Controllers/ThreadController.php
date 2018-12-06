0c89611401e360c8a9f74a1751f67429d876e4fb
sustained <sustained.dissonance@gmail.com>
2018-12-06 09:13:49 +0000

    Change when auth middleware applies.

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -9,7 +9,7 @@ class ThreadController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
@@ -33,8 +33,9 @@ class ThreadController extends Controller
      */
     public function create()
     {
-        //
+        return view('threads.create');
     }
+
 
     /**
      * Store a newly created resource in storage.
