fd920f4e9fe8b2e8097a35825bdfdf040ec06166
sustained <sustained.dissonance@gmail.com>
2018-12-06 01:43:03 +0000

    Add author and replies association.

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function replies()
+    {
+        return $this->hasMany(Reply::class, 'parent_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
