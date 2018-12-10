a252d9de10dd79939ad7abc6bba0e0f70c6addc6
sustained <sustained.dissonance@gmail.com>
2018-12-10 02:48:19 +0000

    Show reply count on thread list.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,7 +10,10 @@
                 <div class="card-body">
                     @foreach($threads as $thread)
                         <article>
-                            <h4><a href="{{ $thread->path() }}">{{ $thread->title }}</a></h4>
+                            <div style="display: flex;">
+                                <h4 style="flex: 1;"><a href="{{ $thread->path() }}">{{ $thread->title }}</a></h4>
+                                <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+                            </div>
 
                             <div class="body">
                                 {{ $thread->body }}
