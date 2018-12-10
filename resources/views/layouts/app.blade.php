2b770ccb1ebe65be4a4ef76d335339d78c36de1f
sustained <sustained.dissonance@gmail.com>
2018-12-10 00:55:27 +0000

    Add dropdown for thread browse modes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/threads">{{ __('All Threads') }}</a>
-                        </li>
+                        <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="browse-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Browse
+                                </a>
+                                
+                                <div class="dropdown-menu" aria-labelledby="browse-dropdown">
+                                    <a class="dropdown-item" href="/threads">All Threads</a>
+                                @auth
+                                    <a class="dropdown-item" href="/threads?author={{ auth()->user()->name }}">My Threads</a>
+                                @endauth
+                                </div>
+                            </li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="categories-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
